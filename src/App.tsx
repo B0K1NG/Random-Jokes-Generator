cae5daabb9614032b2645a210cc17b22706fe992
B0K1NG <dominykaspavlijus@gmail.com>
2025-05-19 17:15:08 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import JokeDisplay from './components/JokeDisplay'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Joke Generator</h1>
+        <JokeDisplay />
+        <footer>
+          <p>Made with ❤️ by B0K1NG</p>
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
